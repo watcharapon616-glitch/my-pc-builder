26d9d878271cc0aaff99d952defa4fca11d4c854
watcharapon616 <watcharapon616@gmail.com>
2026-01-15 21:56:22 +0700

    Final Update: Sync package.json with subdomain

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-pc-builder/', // ต้องมี / ปิดท้ายแบบนี้เท่านั้น
+  base: '/', // แก้เป็น '/' เพราะตอนนี้ builder.zynksite.com คือหน้าแรกสุดของโปรเจกต์นี้ครับ
 })
